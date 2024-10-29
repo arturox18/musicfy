2ff0c8ffaad176b43a41b5ad5e4d959f1610c14b
arturox18 <167452179+arturox18@users.noreply.github.com>
2024-10-28 19:28:10 -0700

    Ya funciona el formulario
    Ya guarda la información en la tabla persona y la tabla usuario

--- formulario_usuario.php
+++ formulario_usuario.php
@@ -3,29 +3,36 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "jc_seguridad";
-
+$dbname = "musica";
+ 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+$nombre = $conn->real_escape_string($_POST['nombre']);
+$apaterno = $conn->real_escape_string($_POST['apaterno']);
+$amaterno = $conn->real_escape_string($_POST['amaterno']);
+$sexo = $conn->real_escape_string($_POST['sexo']);
 
-$nombre = $_POST['nombre'];
-$apellido_paterno = $_POST['apellido_paterno'];
-$apellido_materno = $_POST['apellido_materno'];
-$nick_name = $_POST['nick_name'];
-$contraseña = $_POST['contraseña'];
+$sql_persona = "INSERT INTO persona (nombre, apaterno, amaterno, sexo) VALUES ('$nombre', '$apaterno', '$amaterno', '$sexo')";
 
-$contraseña_cifrada = password_hash($contraseña, PASSWORD_DEFAULT);
+if ($conn->query($sql_persona) === TRUE) {
+    $persona_id = $conn->insert_id;
 
-$sql = "INSERT INTO usuario (nombre, apellido_paterno, apellido_materno, nick_name, contraseña) VALUES ('$nombre', '$apellido_paterno',
- '$apellido_materno', '$nick_name', '$contraseña_cifrada')";
+    $nom_usuario = $conn->real_escape_string($_POST['nom_usuario']);
+    $correo = $conn->real_escape_string($_POST['correo']);
+    $pw = $conn->real_escape_string($_POST['pw']);
 
-if ($conn->query($sql) === TRUE) {
-    echo "Registro exitoso";
+    $sql_usuario = "INSERT INTO usuario (nom_usuario, correo, pw, fk_persona) VALUES ('$nom_usuario', '$correo', '$pw', '$persona_id')";
+
+    if ($conn->query($sql_usuario) === TRUE) {
+        echo "Usuario registrado correctamente.";
+    } else {
+        echo "Error al registrar en la tabla usuario: " . $conn->error;
+    }
 } else {
-    echo "Error en la consulta SQL: " . $conn->error;
+    echo "Error al registrar en la tabla persona: " . $conn->error;
 }
 $conn->close();
 ?>
